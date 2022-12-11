8a89e91ea2cf54f4eb6b73f1395b78675e73c71a
Barel Shraga <barel123133@gmail.com>
2022-12-11 22:39:13 +0200

    ignore unreachable error in build time

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 		<html>
 			<head />
 			<body>
+				{/* @ts-ignore: Unreachable code error*/}
 				<Header />
 				{children}
 			</body>
