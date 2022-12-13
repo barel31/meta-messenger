6cd30ef6b1a32dc5b81ff3242569c5dbf063c203
Barel Shraga <barel123133@gmail.com>
2022-12-13 05:39:06 +0200

    middleware edge-dynamic-code-evaluation fix attemp

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,18 +1,18 @@
-import { getProviders, signIn } from 'next-auth/react';
+import { getProviders } from 'next-auth/react';
 import Image from 'next/image';
 import SignInComponent from './SignInComponent';
 
 export default async function SignInPage() {
 	const providers = await getProviders();
-    
+
 	return (
-		<div className='grid justify-center'>
+		<div className="grid justify-center">
 			<div>
 				<Image
 					className="rounded-full mx-2 object-cover"
 					width={700}
 					height={700}
-					src="https://links.papareact.com/161"
+					src="assets/logo.png"
 					alt="Profile Picture"
 				/>
 			</div>
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,11 @@
 export { default } from 'next-auth/middleware';
 
 // Sucures the matching routes...
-export const config = { matcher: ['/'] };
+export const config = {
+	matcher: ['/'],
+	runtime: 'experimental-edge', // for Edge API Routes only
+	unstable_allowDynamic: [
+		'/lib/utilities.js', // allows a single file
+		'/node_modules/function-bind/**', // use a glob to allow anything in the function-bind 3rd party module
+	],
+};
